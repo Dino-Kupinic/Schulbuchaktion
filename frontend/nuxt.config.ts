97567c6e4a5ce8558623afb0cec16d4a26e77e9b
Neuery17Alt <jangerer@htbla-steyr.ac.at>
2024-03-07 13:54:07 +0100

    installed dependencies

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/i18n",
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxt/test-utils",
+    "nuxt-icon",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@nuxtjs/storybook",
+    "@vite-pwa/nuxt",
+    "nuxt-viewport",
+  ],
+  pinia: {
+    storesDirs: ["./stores/**"],
+  },
 })

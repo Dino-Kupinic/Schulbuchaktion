830d5e73c4ebc8fa9c7c2c6c93b324d44afde608
dsamhabe <dsamhabe@htl-steyr.ac.at>
2024-03-18 09:24:47 +0100

    feat (wip): added container for better Nav Layout

--- frontend/components/nav/NavBar.vue
+++ frontend/components/nav/NavBar.vue
@@ -4,8 +4,8 @@
 
 <template>
   <UContainer
-      class="flex underline-offset-1 w-11/12 h-16 border border-neutral-300 dark:bg-gray-900
-      dark:border-gray-700 items-center rounded-lg ">
+      class="mt-2 flex underline-offset-1 w-[95%] h-11/12 border border-neutral-300 dark:bg-gray-900
+      dark:border-gray-700 items-center box-border rounded-lg shadow-lg">
       <Logo class="h-12"></Logo>
   </UContainer>
 </template>
---
+++ frontend/components/nav/NavBarContainer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UContainer class="box-border flex w-full justify-center h-20"><NavBar/></UContainer>
+</template>
+
+<style scoped>
+
+</style>

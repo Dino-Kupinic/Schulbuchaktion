80b2e925aebefb428364c9625976e0edc114a3af
dsamhabe <dsamhabe@htl-steyr.ac.at>
2024-03-18 09:13:25 +0100

    feat (wip): added basic container for NavBar

---
+++ frontend/components/nav/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UContainer
+      class="flex underline-offset-1 w-11/12 h-16 border border-neutral-300 dark:bg-gray-900
+      dark:border-gray-700 items-center rounded-lg ">
+      <Logo class="h-12"></Logo>
+  </UContainer>
+</template>
+
+<style scoped>
+
+</style>

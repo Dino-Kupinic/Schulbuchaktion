c2aa59ee72606dbc2cba44cd39b6f5da6ae43cae
dsamhabe <dsamhabe@htl-steyr.ac.at>
2024-03-18 09:35:59 +0100

    feat (wip): NavLogo is now link to "/"

--- frontend/components/nav/NavBar.vue
+++ frontend/components/nav/NavBar.vue
@@ -4,9 +4,11 @@
 
 <template>
   <UContainer
-      class="mt-2 flex underline-offset-1 w-[95%] h-11/12 border border-neutral-300 dark:bg-gray-900
+      class="mt-2 flex underline-offset-1 w-[97%] h-11/12 border border-neutral-300 dark:bg-gray-900
       dark:border-gray-700 items-center box-border rounded-lg shadow-lg">
+    <ULink to="/">
       <Logo class="h-12"></Logo>
+    </ULink>
   </UContainer>
 </template>
 

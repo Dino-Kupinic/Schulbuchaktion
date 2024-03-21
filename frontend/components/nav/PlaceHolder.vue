d3315ecd2d4df9a1018fae11c472a866a7cbcf01
dsamhabe <dsamhabe@htl-steyr.ac.at>
2024-03-21 13:17:04 +0100

    feat: added placeholders for development

--- frontend/components/nav/PlaceHolder.vue
+++ frontend/components/nav/PlaceHolder.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <UContainer class="w-48 h-12"></UContainer>
+  <UContainer class="w-48 h-12 items-center"><span class="align-middle">Placeholder</span></UContainer>
 </template>
 
 <style scoped>

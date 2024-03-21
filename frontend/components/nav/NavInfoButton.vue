f208b7f51b06fe7375ac7c8f744cc892f6fa63c5
Neuery17Alt <jangerer@htbla-steyr.ac.at>
2024-03-21 14:02:29 +0100

    feat: added the Info Button for the Navbar

---
+++ frontend/components/nav/NavInfoButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UButton class="dark:bg-primary-800 hover:text-white dark:text-white text-primary-500 bg-primary-100/75 max-w-xs transition duration-300 ease-in-out hover:scale-110">Info</UButton>
+</template>
+
+<style scoped>
+
+</style>

e8b13ceea511e860df2afabfcbc236249145638e
dsamhabe <dsamhabe@htl-steyr.ac.at>
2024-03-21 13:41:42 +0100

    feat (wip): added basic responsiveness

--- frontend/components/nav/NavBar.vue
+++ frontend/components/nav/NavBar.vue
@@ -8,23 +8,24 @@ import PlaceHolder from "~/components/nav/PlaceHolder.vue";
       class="mt-2 flex underline-offset-1 w-[97%] h-11/12 border border-neutral-300 dark:bg-gray-900
       dark:border-gray-700 items-center box-border rounded-lg shadow-lg justify-between"
   >
-    <NavBarLeftSection>
-      <ULink to="/">
-        <Logo class="h-12"></Logo>
-      </ULink>
+    <ULink to="/">
+      <Logo class="h-12"></Logo>
+    </ULink>
+
+    <NavBarLeftSection class="max-md:hidden">
       <PlaceHolder></PlaceHolder>
       <PlaceHolder></PlaceHolder>
       <PlaceHolder></PlaceHolder>
       <PlaceHolder></PlaceHolder>
     </NavBarLeftSection>
 
-    <NavBarRightSection>
+    <NavBarRightSection class="max-lg:hidden">
       <PlaceHolder></PlaceHolder>
       <PlaceHolder></PlaceHolder>
       <PlaceHolder></PlaceHolder>
     </NavBarRightSection>
 
-
+    <NavBarBurgerMenu class="lg:hidden"/>
   </UContainer>
 </template>
 
--- frontend/components/nav/NavBarBurgerMenu.vue
+++ frontend/components/nav/NavBarBurgerMenu.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
-
+const items = [
+  [{
+    label: 'Profile',
+    avatar: {
+      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+    }
+  }], [{
+    label: 'Edit',
+    icon: 'i-heroicons-pencil-square-20-solid',
+    shortcuts: ['E'],
+    click: () => {
+      console.log('Edit')
+    }
+  }, {
+    label: 'Duplicate',
+    icon: 'i-heroicons-document-duplicate-20-solid',
+    shortcuts: ['D'],
+    disabled: true
+  }], [{
+    label: 'Archive',
+    icon: 'i-heroicons-archive-box-20-solid'
+  }, {
+    label: 'Move',
+    icon: 'i-heroicons-arrow-right-circle-20-solid'
+  }], [{
+    label: 'Delete',
+    icon: 'i-heroicons-trash-20-solid',
+    shortcuts: ['⌘', 'D']
+  }]
+]
 </script>
 
 <template>
-
+  <UDropdown :items="items" :popper="{ placement: 'bottom-start' }" class="mx-2" size="xl">
+    <UButton color="blue" trailing-icon="i-heroicons-bars-3-solid" variant: soft/>
+  </UDropdown>
 </template>
 
 <style scoped>

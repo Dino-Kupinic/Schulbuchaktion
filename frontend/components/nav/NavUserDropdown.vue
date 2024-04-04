a4e76aa52f92039dfe573b7fd87b36cc2aeb35c3
Neuer17Alt <jangerer@htl-steyr.ac.at>
2024-04-04 13:11:05 +0200

    feat: added Dropdown when hovering over User-Avatar

---
+++ frontend/components/nav/NavUserDropdown.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const items = [
+  [{
+    label: "Dino Kupinic",
+    slot: "account",
+    disabled: true,
+  }],
+  [{
+    label: "My Books",
+    icon: "i-heroicons-book-open",
+  }, {
+    label: "My Teachers",
+    icon: "i-heroicons-user-group-solid",
+  }],
+  [{
+    label: "Sign out",
+  }],
+]
+</script>
+
+<template>
+  <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }"
+             :popper="{ placement: 'bottom-start',  }">
+    <slot></slot>
+
+    <template #account="{ item }">
+      <div class="text-left">
+        <p class="truncate font-medium text-gray-900 dark:text-white">
+          {{ item.label }}
+        </p>
+      </div>
+    </template>
+
+    <template #item="{ item }">
+      <UIcon v-if="item.label != 'Sign out'" :name="item.icon" class="h-4 w-4 text-gray-400 dark:text-gray-500 "/>
+      <ULink v-if="item.label != 'Sign out'" class="truncate">{{ item.label }}</ULink>
+      <UButton v-else variant="outline" color="red">{{ item.label }}</UButton>
+    </template>
+
+
+  </UDropdown>
+</template>

632991c4c29c40ca95aa491929819c7c96055985
Neuer17Alt <jangerer@htl-steyr.ac.at>
2024-04-04 15:07:29 +0200

    feat: reformated NavUserAvatar.vue file

--- frontend/components/nav/NavUserAvatar.vue
+++ frontend/components/nav/NavUserAvatar.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <UAvatar class="bg-gradient-to-r from-primary-700 to-primary-400 text-white" size="lg">DK</UAvatar>
+  <UAvatar
+    class="from-primary-700 to-primary-400 bg-gradient-to-r text-white"
+    size="lg"
+    >DK</UAvatar
+  >
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>

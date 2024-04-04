718a389d603155c9df129eedae794920a98d0bc2
Neuer17Alt <jangerer@htl-steyr.ac.at>
2024-04-04 13:09:44 +0200

    feat: User Avatar for the Navbar containing a Dropdown and User-Avatar

---
+++ frontend/components/nav/NavUser.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import NavUserDropdown from "~/components/nav/NavUserDropdown.vue"
+</script>
+
+<template>
+  <NavUserDropdown>
+    <NavUserAvatar/>
+  </NavUserDropdown>
+
+</template>
+
+<style scoped>
+
+</style>

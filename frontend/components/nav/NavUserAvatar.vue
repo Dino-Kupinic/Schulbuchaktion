71f0b6ba0c50bb42b4783675d12586e44eb63fac
Neuer17Alt <jangerer@htl-steyr.ac.at>
2024-04-04 13:10:20 +0200

    feat: added icon for User-Avatar

---
+++ frontend/components/nav/NavUserAvatar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UAvatar class="bg-gradient-to-r from-primary-700 to-primary-400 text-white" size="lg">DK</UAvatar>
+</template>
+
+<style scoped>
+
+</style>

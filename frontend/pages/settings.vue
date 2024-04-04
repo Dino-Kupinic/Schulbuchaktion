8d370f0e9c56c5b079f2285def509abef2ddb77a
Neuer17Alt <jangerer@htl-steyr.ac.at>
2024-04-04 13:11:31 +0200

    feat: added a settings-page

--- frontend/pages/settings.vue
+++ frontend/pages/settings.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 
+import NavUserAvatar from "~/components/nav/NavUserAvatar.vue"
+import NavUser from "~/components/nav/NavUser.vue"
 </script>
 
 <template>
     <h1>Settings-Page</h1>
+  <NavUser/>
 </template>
 
 <style scoped>

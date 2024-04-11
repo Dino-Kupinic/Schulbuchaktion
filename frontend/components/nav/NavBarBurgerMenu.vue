18d28e4b032f6c7644dd24845d37e53f797f91a9
dsamhabe <dsamhabe@htl-steyr.ac.at>
2024-04-11 11:35:28 +0200

    feat(wip): work on BurgerMenu height

--- frontend/components/nav/NavBarBurgerMenu.vue
+++ frontend/components/nav/NavBarBurgerMenu.vue
@@ -36,11 +36,10 @@ watch(revealMenu, () => {
     variant="soft"
     @click="openMenu()"
   />
-  <NavBarThemeToggle />
   <template v-if="revealMenu">
     <UContainer
       :class="backgroundColor"
-      class="absolute top-20 z-10 mb-5 box-border flex h-[95%] w-[97%] flex-col items-start justify-start gap-y-5 overflow-x-hidden rounded-b border border-t-0 border-neutral-300 p-5 opacity-100 dark:border-gray-700"
+      class="absolute top-20 z-10 mb-5 box-border flex h-full w-[97%] flex-col items-start justify-start gap-y-5 overflow-x-hidden rounded-b border border-t-0 border-neutral-300 p-5 opacity-100 dark:border-gray-700"
     >
       <PlaceHolder />
       <PlaceHolder />

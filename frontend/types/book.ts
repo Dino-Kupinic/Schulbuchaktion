2724c45f53a58b9e0ee7134a69b777c9d93de9af
Neuer17Alt <jangerer@htl-steyr.ac.at>
2024-04-14 16:17:20 +0200

    feat: added typescript types for every entity

---
+++ frontend/types/book.ts
@@ -0,0 +1,15 @@
+type Book = {
+  id: number
+  orderNumber: number
+  shortTitle: string
+  title: string
+  schoolForm: number
+  description: string
+  bookPrice: number
+  ebook: boolean
+  ebookPlus: boolean
+  subject: Subject
+  publisher: Publisher
+  year: Years[]
+  grade: number
+}

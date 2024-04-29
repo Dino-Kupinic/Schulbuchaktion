20192045a48c92392a9d89d862d0224dd018ca6e
mploierMacBook <mploier@htl-steyr.ac.at>
2024-04-29 08:41:54 +0200

    refactor: rename login route

--- backend/src/Controller/LoginController.php
+++ backend/src/Controller/LoginController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/login', name: 'login.')]
+#[Route("api/v1")]
 class LoginController extends AbstractController
 {
   #[Route(name: 'index')]

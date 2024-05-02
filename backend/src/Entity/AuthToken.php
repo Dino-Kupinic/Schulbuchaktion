5bced84d16961563e8f0f3aeb0371816f495eaaf
mploierMacBook <mploier@htl-steyr.ac.at>
2024-05-02 14:04:57 +0200

    feat: add ENV var for absolut path of secret key

--- backend/src/Entity/AuthToken.php
+++ backend/src/Entity/AuthToken.php
@@ -41,7 +41,7 @@ class AuthToken
   public function __construct(string $username = null, bool $authenticated = null, EntityManagerInterface $em = null)
   {
     if (empty(AuthToken::$key)) {
-      self::$key = file_get_contents("$_SERVER[PWD]/config/jwt/private.pem");
+      self::$key = file_get_contents($_ENV["JWT_SECRET_ABSOLUT_PATH"]);
       self::$timezone = new DateTimeZone("Europe/Vienna");
     }
 

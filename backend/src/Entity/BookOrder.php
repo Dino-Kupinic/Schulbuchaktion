383cdafa4573bb537366a0996e17ec08dd0951b4
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-04 21:23:05 +0200

    feat: small changes

--- backend/src/Entity/BookOrder.php
+++ backend/src/Entity/BookOrder.php
@@ -4,131 +4,137 @@ namespace App\Entity;
 
 use App\Repository\BookOrderRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookOrderRepository::class)]
 class BookOrder
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  #[Groups(['book:read'])]
+  private ?int $id = null;
 
-    #[ORM\Column]
-    private ?int $count = null;
+  #[ORM\Column]
+  #[Groups(['book:read'])]
+  private ?int $count = null;
 
-    #[ORM\Column]
-    private ?bool $teacherCopy = null;
+  #[ORM\Column]
+  #[Groups(['book:read'])]
+  private ?bool $teacherCopy = null;
 
-    #[ORM\ManyToOne(inversedBy: 'bookOrders')]
-    private ?SchoolClass $schoolClass = null;
+  #[ORM\Column(length: 255)]
+  #[Groups(['book:read'])]
+  private ?string $lastUser = null;
 
-    #[ORM\ManyToOne(inversedBy: 'bookOrders')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Book $bookId = null;
+  #[ORM\Column(length: 255)]
+  #[Groups(['book:read'])]
+  private ?string $creationUser = null;
 
-    #[ORM\ManyToOne(inversedBy: 'bookOrders')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Years $year = null;
+  #[ORM\ManyToOne(inversedBy: 'bookOrders')]
+  private ?SchoolClass $schoolClass = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $lastUser = null;
+  #[ORM\ManyToOne(inversedBy: 'bookOrders')]
+  #[ORM\JoinColumn(nullable: false)]
+  private ?Book $bookId = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $creationUser = null;
+  #[ORM\ManyToOne(inversedBy: 'bookOrders')]
+  #[ORM\JoinColumn(nullable: false)]
+  private ?Years $year = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getCount(): ?int
-    {
-        return $this->count;
-    }
+  public function getCount(): ?int
+  {
+    return $this->count;
+  }
 
-    public function setCount(int $count): static
-    {
-        $this->count = $count;
+  public function setCount(int $count): static
+  {
+    $this->count = $count;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function isTeacherCopy(): ?bool
-    {
-        return $this->teacherCopy;
-    }
+  public function isTeacherCopy(): ?bool
+  {
+    return $this->teacherCopy;
+  }
 
-    public function setTeacherCopy(bool $teacherCopy): static
-    {
-        $this->teacherCopy = $teacherCopy;
+  public function setTeacherCopy(bool $teacherCopy): static
+  {
+    $this->teacherCopy = $teacherCopy;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getTeacherCopy(): ?bool
-    {
-      return $this->teacherCopy;
-    }
+  public function getTeacherCopy(): ?bool
+  {
+    return $this->teacherCopy;
+  }
 
-    public function getSchoolClass(): ?SchoolClass
-    {
-        return $this->schoolClass;
-    }
+  public function getSchoolClass(): ?SchoolClass
+  {
+    return $this->schoolClass;
+  }
 
-    public function setSchoolClass(?SchoolClass $schoolClass): static
-    {
-        $this->schoolClass = $schoolClass;
+  public function setSchoolClass(?SchoolClass $schoolClass): static
+  {
+    $this->schoolClass = $schoolClass;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getBookId(): ?Book
-    {
-        return $this->bookId;
-    }
+  public function getBookId(): ?Book
+  {
+    return $this->bookId;
+  }
 
-    public function setBookId(?Book $bookId): static
-    {
-        $this->bookId = $bookId;
+  public function setBookId(?Book $bookId): static
+  {
+    $this->bookId = $bookId;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getYear(): ?Years
-    {
-        return $this->year;
-    }
+  public function getYear(): ?Years
+  {
+    return $this->year;
+  }
 
-    public function setYear(?Years $year): static
-    {
-        $this->year = $year;
+  public function setYear(?Years $year): static
+  {
+    $this->year = $year;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getLastUser(): ?string
-    {
-        return $this->lastUser;
-    }
+  public function getLastUser(): ?string
+  {
+    return $this->lastUser;
+  }
 
-    public function setLastUser(string $lastUser): static
-    {
-        $this->lastUser = $lastUser;
+  public function setLastUser(string $lastUser): static
+  {
+    $this->lastUser = $lastUser;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getCreationUser(): ?string
-    {
-        return $this->creationUser;
-    }
+  public function getCreationUser(): ?string
+  {
+    return $this->creationUser;
+  }
 
-    public function setCreationUser(string $creationUser): static
-    {
-        $this->creationUser = $creationUser;
+  public function setCreationUser(string $creationUser): static
+  {
+    $this->creationUser = $creationUser;
 
-        return $this;
-    }
+    return $this;
+  }
 
 
 }

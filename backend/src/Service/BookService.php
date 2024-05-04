40dca1e262785c89d1841fed5034c843df692b72
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-04 21:23:24 +0200

    feat: rewrite BookService

--- backend/src/Service/BookService.php
+++ backend/src/Service/BookService.php
@@ -16,149 +16,75 @@ use Exception;
  */
 class BookService
 {
-  private EntityManagerInterface $em;
+  private EntityManagerInterface $entityManager;
+  private BookRepository $bookRepository;
 
-  public function __construct(EntityManagerInterface $em)
+  public function __construct(EntityManagerInterface $entityManager, BookRepository $bookRepository)
   {
-    $this->em = $em;
+    $this->entityManager = $entityManager;
+    $this->bookRepository = $bookRepository;
   }
 
   /**
    * Create a new book.
    *
-   * @param Book $book
-   * @return bool True if successful, false otherwise
+   * @param Book $book The book object to persist
+   * @return Book The persisted book object
    * @throws Exception If an error occurs during transaction
    */
-  public function createBook(Book $book): bool
+  public function createBook(Book $book): Book
   {
-    try {
-      $this->em->persist($book);
-      $this->em->flush();
-    } catch (Exception $e) {
-      $this->em->rollback();
-      throw $e;
+    $this->entityManager->persist($book);
+    $this->entityManager->flush();
+    return $book;
+  }
+
+  /**
+   * Update a book.
+   *
+   * @param Book $book The book object with updated information
+   * @return Book The updated book object
+   * @throws Exception If an error occurs during transaction
+   */
+  public function updateBook(Book $book): Book
+  {
+    $this->entityManager->persist($book);
+    $this->entityManager->flush();
+    return $book;
+  }
+
+  /**
+   * Delete a book.
+   *
+   * @param int $id The id of the book to delete
+   * @throws Exception If an error occurs during transaction
+   */
+  public function deleteBook(int $id): void
+  {
+    $book = $this->findBookById($id);
+    if ($book === null) {
+      throw new Exception("Book with id $id not found");
     }
-    return true;
+    $this->entityManager->remove($book);
+    $this->entityManager->flush();
   }
 
-  public function updateBook($book, EntityManagerInterface $em): void
+  /**
+   * Get all books.
+   *
+   * @return array|null An array of all books or null if none found
+   */
+  public function getBooks(): array|null
   {
-    $bookUpdate = $em->getRepository(Book::class)->find($book->getId());
-    $bookUpdate->setOrderNumber($book->getOrderNumber());
-    $bookUpdate->setYear($book->getYear());
-    $bookUpdate->setSubject($book->getSubject());
-    $bookUpdate->setPublisher($book->getPublisher());
-    $bookUpdate->setShortTitle($book->getShortTitle());
-    $bookUpdate->setTitle($book->getTitle());
-    $bookUpdate->setSchoolForm($book->getSchoolForm());
-    $bookUpdate->setDescription($book->getDescription());
-    $bookUpdate->setBookPrice($book->getBookPrice());
-    $bookUpdate->setEbook($book->getEbook());
-    $bookUpdate->setEbookPlus($book->getEbookPlus());
-    $bookUpdate->setGrade($book->getGrade());
-    $em->flush();
+    return $this->bookRepository->findAll();
   }
 
-  public function dropBook($id, EntityManagerInterface $em): void
+  /**
+   * @param int $id The id of the book to retrieve
+   * @return Book|null The book object or null if not found
+   */
+  public function findBookById(int $id): Book|null
   {
-    $book = $em->getRepository(Book::class)->find($id);
-    $em->remove($book);
-    $em->flush();
+    return $this->bookRepository->find($id);
   }
-
-  public function getBooks(BookRepository $bookRepository): array
-  {
-    return $bookRepository->findAll();
-  }
-
-  public function getBookById($id, BookRepository $bookRepository): Book
-  {
-    return $bookRepository->find($id);
-  }
-
-  public function updateBookOrderNumber($book, EntityManagerInterface $em): void
-  {
-    $bookOrderNumber = $em->getRepository(Book::class)->find($book->getId());
-    $bookOrderNumber->setOrderNumber($book->getOrderNumber());
-    $em->flush();
-  }
-
-  public function updateBookYear($book, EntityManagerInterface $em): void
-  {
-    $bookYear = $em->getRepository(Book::class)->find($book->getId());
-    $bookYear->setYear($book->getYear());
-    $em->flush();
-  }
-
-  public function updateBookSubject($book, EntityManagerInterface $em): void
-  {
-    $bookSubject = $em->getRepository(Book::class)->find($book->getId());
-    $bookSubject->setSubject($book->getSubject());
-    $em->flush();
-  }
-
-  public function updateBookPublisher($book, EntityManagerInterface $em): void
-  {
-    $bookPublisher = $em->getRepository(Book::class)->find($book->getId());
-    $bookPublisher->setPublisher($book->getPublisher());
-    $em->flush();
-  }
-
-  public function updateBookShortTitle($book, EntityManagerInterface $em): void
-  {
-    $bookShortTitle = $em->getRepository(Book::class)->find($book->getId());
-    $bookShortTitle->setShortTitle($book->getShortTitle());
-    $em->flush();
-  }
-
-  public function updateBookTitle($book, EntityManagerInterface $em): void
-  {
-    $bookTitle = $em->getRepository(Book::class)->find($book->getId());
-    $bookTitle->setTitle($book->getTitle());
-    $em->flush();
-  }
-
-  public function updateBookSchoolForm($book, EntityManagerInterface $em): void
-  {
-    $bookSchoolForm = $em->getRepository(Book::class)->find($book->getId());
-    $bookSchoolForm->setSchoolForm($book->getSchoolForm());
-    $em->flush();
-  }
-
-  public function updateBookDescription($book, EntityManagerInterface $em): void
-  {
-    $bookDescription = $em->getRepository(Book::class)->find($book->getId());
-    $bookDescription->setDescription($book->getDescription());
-    $em->flush();
-  }
-
-  public function updateBookPrice($book, EntityManagerInterface $em): void
-  {
-    $bookPrice = $em->getRepository(Book::class)->find($book->getId());
-    $bookPrice->setBookPrice($book->getBookPrice());
-    $em->flush();
-  }
-
-  public function updateBookEbook($book, EntityManagerInterface $em): void
-  {
-    $bookEbook = $em->getRepository(Book::class)->find($book->getId());
-    $bookEbook->setEbook($book->getEbook());
-    $em->flush();
-  }
-
-  public function updateBookEbookPlus($book, EntityManagerInterface $em): void
-  {
-    $bookEbookPlus = $em->getRepository(Book::class)->find($book->getId());
-    $bookEbookPlus->setEbookPlus($book->getEbookPlus());
-    $em->flush();
-  }
-
-  public function updateBookGrade($book, EntityManagerInterface $em): void
-  {
-    $bookGrade = $em->getRepository(Book::class)->find($book->getId());
-    $bookGrade->setGrade($book->getGrade());
-    $em->flush();
-  }
-
 }

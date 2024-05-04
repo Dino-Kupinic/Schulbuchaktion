53a12de74b4beb966ccccb2233dbf02ff7724a34
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-04 21:22:39 +0200

    feat: small changes

--- backend/src/Entity/Subject.php
+++ backend/src/Entity/Subject.php
@@ -6,70 +6,73 @@ use App\Repository\SubjectRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: SubjectRepository::class)]
 class Subject
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  #[Groups(['book:read'])]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+  #[ORM\Column(length: 255)]
+  #[Groups(['book:read'])]
+  private ?string $name = null;
 
-    #[ORM\OneToMany(targetEntity: Book::class, mappedBy: 'subject')]
-    private Collection $books;
+  #[ORM\OneToMany(targetEntity: Book::class, mappedBy: 'subject')]
+  private Collection $books;
 
-    public function __construct()
-    {
-        $this->books = new ArrayCollection();
+  public function __construct()
+  {
+    $this->books = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getName(): ?string
+  {
+    return $this->name;
+  }
+
+  public function setName(string $name): static
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Book>
+   */
+  public function getBooks(): Collection
+  {
+    return $this->books;
+  }
+
+  public function addBook(Book $book): static
+  {
+    if (!$this->books->contains($book)) {
+      $this->books->add($book);
+      $book->setSubject($this);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeBook(Book $book): static
+  {
+    if ($this->books->removeElement($book)) {
+      // set the owning side to null (unless already changed)
+      if ($book->getSubject() === $this) {
+        $book->setSubject(null);
+      }
     }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Book>
-     */
-    public function getBooks(): Collection
-    {
-        return $this->books;
-    }
-
-    public function addBook(Book $book): static
-    {
-        if (!$this->books->contains($book)) {
-            $this->books->add($book);
-            $book->setSubject($this);
-        }
-
-        return $this;
-    }
-
-    public function removeBook(Book $book): static
-    {
-        if ($this->books->removeElement($book)) {
-            // set the owning side to null (unless already changed)
-            if ($book->getSubject() === $this) {
-                $book->setSubject(null);
-            }
-        }
-
-        return $this;
-    }
+    return $this;
+  }
 }

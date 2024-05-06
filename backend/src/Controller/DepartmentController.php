d87835ccdfbbbd7fe71a8d536540551462ca41ce
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-06 16:30:52 +0200

    feat: small changes to department controller

--- backend/src/Controller/DepartmentController.php
+++ backend/src/Controller/DepartmentController.php
@@ -3,21 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Department;
-use App\Entity\User;
-use App\Repository\DepartmentRepository;
 use App\Service\DepartmentService;
-use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
-use function PHPUnit\Framework\isEmpty;
 
-/*
- * This controller is either used to get all departments or a department by id
+/**
+ * Controller class for handling book orders.
+ * @author Lukas Bauer, Dino Kupinic
+ * @version 1.0
+ * @see Department
+ * @see DepartmentService
+ * @see DepartmentRepository
  */
-
 #[Route("/api/v1/departments")]
 class DepartmentController extends AbstractController
 {
@@ -37,7 +38,7 @@ class DepartmentController extends AbstractController
     } catch (Exception $e) {
       return $this->json([
         "success" => false,
-        "error" => "Failed to get departments: " . $e->getMessage()
+        "error" => "Failed to get departments: " . $e->getMessage(),
       ], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
   }
@@ -50,7 +51,7 @@ class DepartmentController extends AbstractController
       ->toArray();
 
     try {
-      $department = $departmentService->getDepartmentById($id);
+      $department = $departmentService->findDepartmentById($id);
       if ($department == null) {
         return $this->json(["success" => true, "data" => []], status: Response::HTTP_NOT_FOUND);
       }
@@ -58,39 +59,45 @@ class DepartmentController extends AbstractController
     } catch (Exception $e) {
       return $this->json([
         "success" => false,
-        "error" => "Failed to get department: " . $e->getMessage()
+        "error" => "Failed to get department with id $id: " . $e->getMessage(),
       ], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
   }
 
   #[Route(path: "/create", methods: ["POST"])]
-  public function createDepartment(Department $department, DepartmentService $departmentService): Response
+  public function createDepartment(DepartmentService $departmentService, Request $request): Response
   {
     $context = (new ObjectNormalizerContextBuilder())
       ->withGroups("department:read")
       ->toArray();
 
     try {
-      $department = $departmentService->createDepartment($department);
+      $temp = $departmentService->parseRequestData($request);
+      $department = $departmentService->createDepartment($temp);
       return $this->json(["success" => true, "data" => $department], status: Response::HTTP_CREATED, context: $context);
     } catch (Exception $e) {
       return $this->json([
         "success" => false,
-        "error" => "Failed to create department: " . $e->getMessage()
+        "error" => "Failed to create department: " . $e->getMessage(),
       ], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
   }
 
-  #[Route(path: "/delete/{id}", methods: ["PUT"])]
-  public function deleteDepartment(DepartmentService $departmentService, int $id): Response
+  #[Route(path: "/update/{id}", methods: ["PUT"])]
+  public function updateDepartment(DepartmentService $departmentService, Request $request, int $id): Response
   {
+    $context = (new ObjectNormalizerContextBuilder())
+      ->withGroups("department:read")
+      ->toArray();
+
     try {
-      $departmentService->deleteDepartment($id);
-      return $this->json(["success" => true, "message" => "Department was successfully deleted"], status: Response::HTTP_OK);
+      $temp = $departmentService->parseRequestData($request);
+      $department = $departmentService->updateDepartment($id, $temp);
+      return $this->json(["success" => true, "data" => $department], status: Response::HTTP_OK, context: $context);
     } catch (Exception $e) {
       return $this->json([
         "success" => false,
-        "error" => "Failed to delete department with id $id: " . $e->getMessage()
+        "error" => "Failed to update department: " . $e->getMessage(),
       ], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
   }

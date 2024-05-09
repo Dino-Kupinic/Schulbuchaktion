6c9472ced77a3f35638346645c3bcc16523125a3
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-09 20:01:23 +0200

    feat: add types

---
+++ frontend/types/year.ts
@@ -0,0 +1,19 @@
+/**
+ * Type for Year
+ * @type Year
+ * @property {number} id - Year id
+ * @property {number} year - Year
+ */
+export type Year = {
+  id: number
+  year: number
+}
+
+/**
+ * Type for YearDTO
+ * @type YearDTO
+ * @property {number} year - Year
+ */
+export type YearDTO = {
+  year: number
+}

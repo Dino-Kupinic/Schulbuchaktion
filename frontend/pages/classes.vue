bf9add23ff9869f00c4430a15319a62efe529ed7
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-27 10:31:50 +0200

    feat(wip): work on classes page

--- frontend/pages/classes.vue
+++ frontend/pages/classes.vue
@@ -1,5 +1,74 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const people = [
+  {
+    id: 1,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+  },
+  {
+    id: 2,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+  },
+  {
+    id: 3,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+  },
+  {
+    id: 4,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+  },
+  {
+    id: 5,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+  },
+  {
+    id: 6,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+  },
+]
+</script>
 
-<template></template>
+<template>
+  <div class="flex h-full w-full flex-col">
+    <PageHeader
+      class="grow-0"
+      :title="$t('classes.title')"
+      :subtitle="$t('classes.subtitle')"
+    />
+    <UCard
+      class="h-auto w-full rounded-lg"
+      :ui="{
+        body: {
+          padding: '',
+        },
+        header: {
+          padding: 'sm:px-4 py-3',
+        },
+      }"
+    >
+      <div class="flex flex-col sm:flex-row">
+        <div class="w-96 sm:border-r">a</div>
+        <UTable :rows="people" class="w-full" />
+      </div>
+    </UCard>
+  </div>
+</template>
 
 <style scoped></style>

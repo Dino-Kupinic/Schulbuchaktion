ea009b908efc560ff0c0c8b72fb71673098f5f43
mploierMacBook <mploier@htl-steyr.ac.at>
2024-05-28 15:51:37 +0200

    feat: add dev environment check

--- backend/src/Middleware/SecurityMiddleware.php
+++ backend/src/Middleware/SecurityMiddleware.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Middleware;
 
 use App\Service\AuthService;
 use App\Service\RouteMatcherService;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
@@ -26,22 +28,25 @@ class SecurityMiddleware implements EventSubscriberInterface
 
   public function onKernelRequest(RequestEvent $event): void
   {
-    $request = $event->getRequest();
-    $requestUri = $request->getRequestUri();
-    $paramPos = strpos($request->getRequestUri(), '?');
-    $requestUri = $paramPos ? substr($requestUri, 0, $paramPos) : $requestUri;
+    if (strcmp($_ENV['APP_ENV'], 'dev') != 0) {
+      $request = $event->getRequest();
+      $requestUri = $request->getRequestUri();
+      $paramPos = strpos($request->getRequestUri(), '?');
+      $requestUri = $paramPos ? substr($requestUri, 0, $paramPos) : $requestUri;
 
-    $parameter = $this->routerService->getRouteNameFromUrl($requestUri);
 
-    if (!(strcmp($requestUri, '/api/v1/login') == 0)) {
-      $bearer = $request->cookies->get('bearer');
+      $parameter = $this->routerService->getRouteNameFromUrl($requestUri);
 
-      if (!$this->authService->checkToken($bearer)) {
-        $response = new Response('Token not valid', Response::HTTP_UNAUTHORIZED);
-        $event->setResponse($response);
-      } else if (!$this->authService->checkRoutePermission($parameter, $bearer)){
-        $response = new Response('Not permitted for this Action', Response::HTTP_UNAUTHORIZED);
-        $event->setResponse($response);
+      if (!(strcmp($requestUri, '/api/v1/login') == 0)) {
+        $bearer = $request->cookies->get('bearer');
+
+        if (!$this->authService->checkToken($bearer)) {
+          $response = new Response('Token not valid', Response::HTTP_UNAUTHORIZED);
+          $event->setResponse($response);
+        } else if (!$this->authService->checkRoutePermission($parameter, $bearer)) {
+          $response = new Response('Not permitted for this Action', Response::HTTP_UNAUTHORIZED);
+          $event->setResponse($response);
+        }
       }
     }
   }

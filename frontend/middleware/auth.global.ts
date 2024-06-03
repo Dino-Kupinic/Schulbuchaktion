3bc59147a5764cc793a14c214755947203a0563a
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-06-03 11:52:32 +0200

    feat: remove console.log and add comments

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -1,16 +1,23 @@
 import { jwtDecode } from "jwt-decode"
 import type { JwtAuthPayload } from "~/types/auth"
 
-function decodeAuthCookie() {
+/**
+ * Decode the auth cookie and return the payload or null
+ */
+function decodeAuthCookie(): JwtAuthPayload | null {
   const token = useCookie("BearerToken")
   return token.value ? jwtDecode<JwtAuthPayload>(token.value) : null
 }
 
+/**
+ * Middleware to check if the user is authenticated
+ * and redirect to the login page if not
+ * @param to - The route to navigate to
+ * @param from - The route the user is coming from
+ */
 export default defineNuxtRouteMiddleware((to, from) => {
   const cookie = decodeAuthCookie()
   const authenticated = cookie?.authenticated
-
-  console.log(authenticated)
 
   if (!authenticated && to.path !== "/login") {
     return navigateTo("/login")

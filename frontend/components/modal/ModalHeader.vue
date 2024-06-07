da074f4cb8905d3014dd5c5f34b53c588dfcfd72
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-06-07 10:37:35 +0200

    feat: add modal header

---
+++ frontend/components/modal/ModalHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  icon: string
+}>()
+</script>
+
+<template>
+  <div class="flex items-center">
+    <Icon :name="icon" class="mr-2 text-red-500 dark:text-red-200" />
+    <p class="font-semibold">{{ title }}</p>
+  </div>
+</template>

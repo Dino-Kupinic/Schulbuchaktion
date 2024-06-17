f93649b402d19c785bd2cc8e43149b951888fda0
mploierMacBook <mploier@htl-steyr.ac.at>
2024-06-17 15:34:40 +0200

    feat: every console output goes into dev/null

--- backend/docker/frankenphp/docker-entrypoint.sh
+++ backend/docker/frankenphp/docker-entrypoint.sh
@@ -53,8 +53,6 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		fi
 	fi
 
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 fi
 
-exec docker-php-entrypoint "$@"
+exec docker-php-entrypoint "$@" > /dev/null 2>&1
